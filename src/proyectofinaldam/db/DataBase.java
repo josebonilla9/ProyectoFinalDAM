5586df52c4da6c04b4c8994cbb76ef214ca15eb2
josebonilla9 <jose1999001@hotmail.com>
2024-05-08 13:54:32 +0200

    Añadiendo comentarios al código

--- src/proyectofinaldam/db/DataBase.java
+++ src/proyectofinaldam/db/DataBase.java
@@ -25,6 +25,7 @@ public class DataBase {
     
     private java.sql.Date fechaSQL;
     
+    //Método para la conexión a la base de datos
     public void conectar(String nombreDB) {
         try {
             conexion = DriverManager.getConnection(URL + nombreDB, USER, PWD);
@@ -34,15 +35,18 @@ public class DataBase {
         }
     }
     
+    //Método para obtener la fecha
     public void obtenerFecha(GraphicInterface gui) {
         Date fechaElegida = gui.getDate();
         fechaSQL = new java.sql.Date(fechaElegida.getTime());
     }
     
+    //Método para insertar citas nuevas en la base de datos
     public void InsertarCitas(int estado, Date fecha, LocalTime hora, String evento, String notas) {
         fecha = fechaSQL;
         
         try {
+            //Se busca el id más alto para que al añadir una tarea se sume 1 
             String queryMax = "SELECT MAX(id) AS id_nuevo FROM citas";
             PreparedStatement statement = conexion.prepareStatement(queryMax);
             ResultSet resultado = statement.executeQuery();
@@ -54,6 +58,7 @@ public class DataBase {
             
             int idNuevo = idMayor + 1;
             
+            //Insertamos todos los datos en la base de datos en la tabla citas
             String query = "INSERT INTO citas (Estado, Fecha, Hora, Evento, Notas, id) VALUES (?, ?, ?, ?, ?, ?)";
             PreparedStatement queryInsert = conexion.prepareStatement(query);
             queryInsert.setInt(1, estado);
@@ -71,10 +76,12 @@ public class DataBase {
         }
     }
     
+    //Método para insertar tares nuevas en la base de datos
     public void InsertarTareas(int estado, Date fecha, LocalTime hora, String evento, String notas) {
         fecha = fechaSQL;
         
         try {
+            //Se busca el id más alto para que al añadir una tarea se sume 1 
             String queryMax = "SELECT MAX(id) AS id_nuevo FROM tareas";
             PreparedStatement statement = conexion.prepareStatement(queryMax);
             ResultSet resultado = statement.executeQuery();
@@ -86,6 +93,7 @@ public class DataBase {
             
             int idNuevo = idMayor + 1;
             
+            //Insertamos todos los datos en la base de datos en la tabla tareas
             String query = "INSERT INTO tareas (Estado, Fecha, Hora, Evento, Notas, id) VALUES (?, ?, ?, ?, ?, ?)";
             PreparedStatement queryInsert = conexion.prepareStatement(query);
             queryInsert.setInt(1, estado);
@@ -103,8 +111,10 @@ public class DataBase {
         }
     }
     
+    //Método para ver las citas en una tabla
     public JTable VerCitas(GraphicInterface gui) {
         
+        //Creación de la tabla
         JTable jTableCitas = gui.getJTableCitas();
          DefaultTableModel model = new DefaultTableModel() {
             @Override
@@ -115,7 +125,8 @@ public class DataBase {
 
         Date fechaElegida = gui.getDate();
         fechaSQL = new java.sql.Date(fechaElegida.getTime());
-
+        
+        //Se añaden las columnas a la tabla
         model.addColumn("Estado");
         model.addColumn("Hora");
         model.addColumn("Evento");
@@ -124,12 +135,14 @@ public class DataBase {
 
         jTableCitas.setModel(model);
         
+        //Se edita el ancho de cada columna según su contenido
         jTableCitas.getColumnModel().getColumn(2).setPreferredWidth(200);
         jTableCitas.getColumnModel().getColumn(3).setPreferredWidth(600);
         jTableCitas.getColumnModel().getColumn(4).setMinWidth(0);
         jTableCitas.getColumnModel().getColumn(4).setMaxWidth(0);
         jTableCitas.getColumnModel().getColumn(4).setWidth(0);
-
+        
+        //Se centra el contenido de cada celda
         DefaultTableCellRenderer headerRenderer = (DefaultTableCellRenderer) jTableCitas.getTableHeader().getDefaultRenderer();
         headerRenderer.setHorizontalAlignment(SwingConstants.CENTER);
 
@@ -139,11 +152,13 @@ public class DataBase {
         
         Object[] dato = new Object[5];
         
+        //Se añade un MouseListener para que al pulsar en la primera celda se cambie de estado de hecho (un tick) a no hecho (una cruz)
         jTableCitas.addMouseListener(new MouseAdapter() {
             
             @Override
             public void mouseClicked(MouseEvent e) {
-
+                
+                //Obtienen la columna y fila en la que se hizo click
                 int row = jTableCitas.rowAtPoint(e.getPoint());
                 int column = jTableCitas.columnAtPoint(e.getPoint());
 
@@ -151,7 +166,8 @@ public class DataBase {
                     DefaultTableModel model = (DefaultTableModel) jTableCitas.getModel();
                     String currentState = (String) model.getValueAt(row, column);
                     int idTabla = (int) model.getValueAt(row, column + 4);
-
+                    
+                    //Según el símbolo que tenga si se pulsa se cambia por el otro, lo que hace que en la base de datos también cambie
                     if (currentState.equals("\u2713")) {
                         model.setValueAt("\u2717", row, column);
                         actualizarEstadoCitas(idTabla, 0);
@@ -164,11 +180,14 @@ public class DataBase {
             }
         });
         
+        //Se busca en la tabla citas de la base de datos la diferente información que mostrar en la tabla con SELECT
         try {
             String query = "SELECT Estado, Hora, Evento, Notas, id FROM citas WHERE Fecha = '" + fechaSQL + "' ";
             PreparedStatement instruccion = conexion.prepareStatement(query);
             ResultSet resultado = instruccion.executeQuery();
             
+            //En la base de datos el estado está añadido con 1 si está hecha la tarea y 0 si no lo está
+            //por lo que aquí si tiene un 1 ponemos un tick y si la base de datos tiene un 0 ponemos una cruz
             while (resultado.next()) {
                 int estado = resultado.getInt("Estado");
                 if (estado != 0) {
@@ -177,13 +196,14 @@ public class DataBase {
                     dato[0] = "\u2717";
                 }
                 
+                //Pasamos el formato de la hora de la base de datos a la que nosotros necesitamos
                 Time horaSQL = resultado.getTime("Hora");
                 LocalTime hora = horaSQL.toLocalTime();
                 int horas = hora.getHour();
                 int minutos = hora.getMinute();
                 String horaFormateada = String.format("%02d:%02d", horas, minutos);
-                dato[1] = horaFormateada;
                 
+                dato[1] = horaFormateada;
                 dato[2] = resultado.getString("Evento");
                 dato[3] = resultado.getString("Notas");
                 dato[4] = resultado.getInt("id");
